c9d74c2e4c1b1bc95cc045611a11218780d894d6
DkmCodder <80331350+dkm13@users.noreply.github.com>
2021-07-16 18:45:33 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let img = document.getElementById("img");
+
+function firstImg() {
+    img.src = "https://th.bing.com/th/id/R.e736c6193713d3ffd67f8732b896daac?rik=9kDEevTJsYbQBg&pid=ImgRaw";
+}
+function secondImg() {
+    img.src = "https://th.bing.com/th/id/R.5bf0cd83d0502f8b5ea223c0fc5e0669?rik=YvRDTDn3Jum%2fGA&riu=http%3a%2f%2fnytransport.blazonco.com%2fimages%2ftransparent-truck.png&ehk=9ImRvOOHXdwGrqRzyX3QbQd9FCCXObl1f4jSdaEWzmU%3d&risl=&pid=ImgRaw";
+}
+function thirdImg() {
+    img.src = "https://th.bing.com/th/id/R.183cbf518426e06438056c2638005e03?rik=mO1TQPaAP2L29Q&riu=http%3a%2f%2fwww.burhanishipping.com%2fwp-content%2fuploads%2f2014%2f05%2fLand-transport-1.png&ehk=OKRsMU7XUPsvRFBVcIefVvE%2fWAeOapvMdEjq%2ba%2bsZNE%3d&risl=&pid=ImgRaw";
+}
+
+
+function first() {
+    var x = document.getElementById('heading');
+    x.innerHTML = "The best Company";
+}
+
+
+function second() {
+    var x = document.getElementById('heading');
+    x.innerHTML = "Leading worldwide Company";
+}
+
+function third() {
+    var x = document.getElementById('heading');
+    x.innerHTML = "Over 43 countries in the world!";
+
+}
